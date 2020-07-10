0f46ce6819eda1364897fc9bfb4adf7ef4bfed2e
Luke Devonshire <luke@MacBook-Pro.local>
2020-07-10 15:59:28 +0100

    Added automation account output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "log_analytics_workspace" {
   value = azurerm_log_analytics_workspace.log_analytics
 }
+
+output "automation_account" {
+  value = azurerm_automation_account.automation_account
+}
